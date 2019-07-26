bed6daa0e7921f217301c774778d3610acf0d8ad
Jason Belgard <belgard.jason@gmail.com>
2019-07-26 13:25:09 -0500

    Deployed to heroku MVP met

--- StartHere/src/main/java/com/lambdaschool/starthere/config/AuthorizationServerConfig.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/config/AuthorizationServerConfig.java
@@ -14,10 +14,10 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
 {
-    static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
-    static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
-    // static final String CLIENT_ID = "lambda-client";
-    // static final String CLIENT_SECRET = "lambda-secret";
+//    static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
+//    static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
+    static final String CLIENT_ID = "lambda-client";
+    static final String CLIENT_SECRET = "lambda-secret";
 
     static final String GRANT_TYPE_PASSWORD = "password";
     static final String AUTHORIZATION_CODE = "authorization_code";
@@ -43,13 +43,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     {
         //                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT)
 
-        configurer.inMemory().withClient(CLIENT_ID).secret(encoder.encode(CLIENT_SECRET)).authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, IMPLICIT).scopes(SCOPE_READ, SCOPE_WRITE, TRUST).accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+        configurer.inMemory()
+                .withClient(CLIENT_ID)
+                .secret(encoder.encode(CLIENT_SECRET))
+                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, IMPLICIT)
+                .scopes(SCOPE_READ, SCOPE_WRITE, TRUST)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS)
+                .refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception
     {
         endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
-        endpoints.pathMapping("/oauth/token", "/login");
+//        endpoints.pathMapping("/oauth/token", "/login");
     }
 }
